4d716aac262687e518ca078eaa34a9666f89f4f7
Lee <leturton1@gmail.com>
2026-08-26 02:37:52 +0000

    External agent changes

--- src/components/voice/GlobalVoiceControl.jsx
+++ src/components/voice/GlobalVoiceControl.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useVoiceSynthesis } from "@/hooks/useVoiceSynthesis";
 
 export default function GlobalVoiceControl() {
